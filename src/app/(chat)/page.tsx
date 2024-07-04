406cd788ad583b5bcf216cebce23016a670e0615
bici-dev <99614711+aphxtwin@users.noreply.github.com>
2024-07-04 20:35:30 -0300

    lc

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -5,11 +5,8 @@ import { redirect } from "next/navigation";
 import {Session} from "@/lib/types";
 
 export default async function ChatPage() {
-    const session = (await auth()) as Session
+    const session = (await auth) as Session
 
-    if (!session?.user) {
-        redirect(`/signup`)
-    }
 
     return (
         <BuildYourForm session={session} />
